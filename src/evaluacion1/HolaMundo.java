4c389a86fabea2199aee907696277cdec859b3f4
davidcp00 <144950983+davidcp00@users.noreply.github.com>
2023-09-13 20:57:51 +0200

    Initial commit

---
+++ src/evaluacion1/HolaMundo.java
@@ -0,0 +1,13 @@
+package evaluacion1;
+/* programa holamundo*/
+public class HolaMundo {
+	public static void main(String[] args) {
+		/* lo único que hace este programa es mostrar
+		 la cadena "Hola Mundo" por pantalla*/
+	
+	System.out.println("Hola Mundo");
+	System.out.println("Hola Mundo");
+
+	}
+
+}
